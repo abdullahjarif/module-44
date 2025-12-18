e32097959c82019a2100e18d6a8fdd03f8e46321
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-12-18 20:07:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
+import Others from "./Component/Others/Others";
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Others></Others>
+    </>
+  );
+}
+
+export default App;
